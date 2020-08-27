fed1de74e38ddebb37536dd9cb7be28febe7c4f5
Sonish Maharjan <sonishmaharjan1@gmail.com>
2020-08-28 00:14:59 +0545

    added route to serve static assets

--- app.js
+++ app.js
@@ -6,17 +6,26 @@ const errorController = require("./controllers/errorController");
 const userRoutes = require("./routes/userRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 const postRoutes = require("./routes/postRoutes");
-const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
-// app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/profiles", profileRoutes);
 app.use("/api/posts", postRoutes);
+
+// SERVE STATIC ASSETS IN PRODUCTION
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.all("*", (req, res, next) => {
   console.log(req.url);
